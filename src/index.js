e6b049671e496d45ba2647a6d373fa2672dcd209
Denis Falkowski <falkowski.denis@gmail.com>
2020-09-28 21:30:49 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        function compareNumbers(a, b) {
+            return a - b;
+        }
+        return array.sort(compareNumbers)[0];
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (typeof array !== 'undefined' && array.length > 0) {
+    function compareNumbers(a, b) {
+        return b - a;
+    }
+    return array.sort(compareNumbers)[0];
+} else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  let calculateAvg = 0;
+  if (typeof array !== 'undefined' && array.length > 0) {
+    for (i = 0; i < array.length; i++) {
+      calculateAvg += array [i];
+    } return calculateAvg / array.length;  
+  } else return 0;
+};
